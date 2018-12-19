ca79e98a08e93c3c13df90346eb49f2a04bcc5a3
Craig Myles <cr@igmyles.com>
2018-12-20 08:11:12 +1100

    Added full CRUD lifecycle

--- client/src/components/Catalogue.vue
+++ client/src/components/Catalogue.vue
@@ -29,10 +29,16 @@ export default {
   }),
   // Methods
   methods: {
+    /**
+     * Handle link click.
+     */
     linkClick: function(event) {
       event.preventDefault();
       alert("Link click disabled for this exercise");
     },
+    /**
+     * Create product.
+     */
     createProduct: async function() {
       // Make a request to create our new product
       const response = await fetch("http://localhost:3000/products", {
@@ -41,16 +47,50 @@ export default {
           "Content-Type": "application/json",
           Accept: "application/json"
         },
-        body: JSON.stringify({ id: 5, name: "New Product" })
+        body: JSON.stringify({ id: "5", name: "New Product" })
       });
       // Parse the json object
       const products = await response.json();
       // Update our local `state`
       this.products = products;
     },
-    getProducts: async function() {
+    /**
+     * Read product listing.
+     */
+    readProducts: async function() {
       // Make a request to get our products
       const response = await fetch("http://localhost:3000/products");
+      // Parse the json object
+      const products = await response.json();
+      // Update our local `state`
+      this.products = products;
+    },
+    /**
+     * Update product.
+     */
+    updateProduct: async function() {
+      // Make a request to update our new product
+      const response = await fetch("http://localhost:3000/products/5", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        },
+        body: JSON.stringify({ id: "5", name: "New Product New Name" })
+      });
+      // Parse the json object
+      const products = await response.json();
+      // Update our local `state`
+      this.products = products;
+    },
+    /**
+     * Delete product.
+     */
+    deleteProduct: async function() {
+      // Make a request to delete our new product
+      const response = await fetch("http://localhost:3000/products/5", {
+        method: "DELETE"
+      });
       // Parse the json object
       const products = await response.json();
       // Update our local `state`
@@ -59,8 +99,14 @@ export default {
   },
   // Created lifehook
   created: async function() {
-    // this.getProducts();
-    this.createProduct();
+    // Get the product listing
+    await this.readProducts();
+    // Create a new product
+    await this.createProduct();
+    // Update a product
+    await this.updateProduct();
+    // Delete this newly created product
+    await this.deleteProduct();
   }
 };
 </script>
--- server/routes/products.js
+++ server/routes/products.js
@@ -2,20 +2,34 @@ var express = require("express");
 var router = express.Router();
 
 var products = [
-  { id: 1, name: "Risk Certification Training" },
-  { id: 2, name: "Standards & Legislation" },
-  { id: 3, name: "Assurance Audit" },
-  { id: 4, name: "Property Decision Making" }
+  { id: "1", name: "Risk Certification Training" },
+  { id: "2", name: "Standards & Legislation" },
+  { id: "3", name: "Assurance Audit" },
+  { id: "4", name: "Property Decision Making" }
 ];
 
-/* POST product. */
-router.post("/", function(req, res, next) {
+/* Create (POST) product. */
+router.post("/", (req, res, next) => {
   products.push(req.body);
   res.send(products);
 });
 
-/* GET products listing. */
-router.get("/", function(req, res, next) {
+/* Read (GET) products. */
+router.get("/", (req, res, next) => {
+  res.send(products);
+});
+
+/* Update (PUT) product. */
+router.put("/:id", (req, res, next) => {
+  products = products.map(product =>
+    product.id === req.params.id ? req.body : product
+  );
+  res.send(products);
+});
+
+/* Delete (DELETE) product. */
+router.delete("/:id", (req, res, next) => {
+  products = products.filter(product => product.id !== req.params.id);
   res.send(products);
 });
 
